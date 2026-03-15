0926214180b07b28b2c38524934ee65556e0e7d0
xlauro <laurohmsantos@gmail.com>
2026-03-14 21:37:06 -0300

    chore(init): bootstrap spring boot project skeleton

---
+++ src/main/java/com/laurosantos/authspring/AuthSpringApplication.java
@@ -0,0 +1,13 @@
+package com.laurosantos.authspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthSpringApplication.class, args);
+    }
+
+}
